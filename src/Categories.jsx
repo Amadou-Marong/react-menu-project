642f873f8019825df89c4c89d9103f3d5c409ba3
Amadou-Marong <marongamadou6@gmail.com>
2024-04-30 11:34:52 +0000

    added filter items

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,9 +1,12 @@
-const Categories = ({categories}) => {
+const Categories = ({categories, filterItems}) => {
+    // const filterItems = (category) => {
+    //     console.log(category);
+    // }
     return (
         <div className="btn-container">
             {categories.map((category) => {
                 return (
-                    <button type="button" className="btn" key={category}>
+                    <button type="button" className="btn" key={category} onClick={() => filterItems(category)}>
                         {category}
                     </button>
                 )

8e5841c4ad19cac7b6a98722cbe8302d1fc39c05
Amadou-Marong <marongamadou6@gmail.com>
2024-04-30 09:13:16 +0000

    added unique categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
-const tempSet = new Set(menu.map((item) => item.category));
-const categories = [...tempSet];
-console.log(categories);
-
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+
+  console.log(categories);
   
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu"/>
+        <Categories categories={categories}/>
         <Menu items={menuItems}/>
       </section>
     </main>
